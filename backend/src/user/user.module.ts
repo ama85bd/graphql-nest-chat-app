d2c38dabe7a4b4843abdd3617b2c73d0af13325f
Asif Mohammad Ashique <asifm.ashique@petronas.com>
2025-03-14 23:18:09 +0600

    FE and BE

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [UserResolver, UserService]
+  providers: [UserResolver, UserService, JwtService, PrismaService],
 })
 export class UserModule {}
